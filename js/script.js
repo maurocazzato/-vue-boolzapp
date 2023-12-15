2d9bd48a2b5e6882fa0fe8eda17206bb2de7febf
Maurocazzato <maurizzio.125@gmail.com>
2023-12-15 10:22:54 +0100

    correzzione funzione sand messgae

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
+// Milestone 1
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+// Milestone 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
+
+// Milestone 3
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+// Milestone 4
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+
+
+
 new Vue({
     el: '#app',
     data: {
+        // contacts che contiene gli oggetti rappresentanti i contatti
       contacts: [
         {
           name: 'Michele',
@@ -164,10 +183,14 @@ new Vue({
             ],
         }
       ],
+    //   activeContact tiene traccia del contatto attualmente selezionato
       activeContact: null,
+    //   variabile  per il testo del nuovo messaggio
       newMessage: '',
+    //    una variabile per la stringa di ricerca.
       search: '',
     },
+    // filteredContacts restituisce un array di contatti filtrati in base alla stringa di ricerca.
     computed: {
       filteredContacts() {
         return this.contacts.filter((contact) =>
@@ -176,9 +199,11 @@ new Vue({
       },
     },
     methods: {
+        // selectContact viene chiamata quando un contatto viene selezionato
       selectContact(contact) {
         this.activeContact = contact;
       },
+    //   sendMessage viene chiamata quando l'utente invia un messaggio.
       sendMessage() {
         if (this.newMessage.trim() === '') return;
   
